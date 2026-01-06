3467b885700c30aa2131bed7e81c8946e8cc013a
JGuzman23 <juanreneguzman23@gmail.com>
2026-01-06 15:11:04 -0400

    fix subir capacidad de adjunto

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,8 @@ app.use(cors({
 }));
 
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
 // Servir archivos estáticos ANTES del tenantMiddleware para que las imágenes sean accesibles
